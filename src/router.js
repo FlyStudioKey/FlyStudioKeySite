c5a204301fe81abea03594fdfa886e3fe3cdbe68
weiwei-cool <2769694799@qq.com>
2023-09-25 00:38:50 +0800

    v0.0.0-alpha.6 fix the bug

--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ import Home from './views/Home.vue';
 import About from './views/About.vue';
 import Feedback from './views/Feedback.vue';
 import Qmcl from './views/app/Qmcl.vue';
-import EULA from "@/views/app/EULA.vue";
+import EULA from "./views/app/EULA.vue";
 
 const routes = [
     {
