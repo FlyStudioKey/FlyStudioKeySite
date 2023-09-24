ce0cd307de5e0826aaeac43e3849aa542b0da34e
weiwei-cool <2769694799@qq.com>
2023-09-24 21:03:58 +0800

    v0.0.0-alpha.2 Fix the bug causing rendering issues on mobile devices.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,9 @@ import Navbar from "@/components/Navbar.vue";
 import Body_LIRT from "@/components/Body_LIRT.vue";
 import BodyM from "@/components/BodyM.vue";
 import Footer from "@/components/Footer.vue";
+import MobileDetect from 'mobile-detect';
 document.title = "FlyStudioKey";
+const md = new MobileDetect(navigator.userAgent);
 </script>
 
 <template>
@@ -14,18 +16,24 @@ document.title = "FlyStudioKey";
 
   <main>
     <br />
-    <BodyM message='<h1>FlyStudioKey | 飞屋软件技术</h1>
+    <BodyM message='<h1 class="fs-2" style="font-weight: bold;">FlyStudioKey | 飞屋软件技术</h1>
                     <br />
                     <p class="fs-4">&emsp;&emsp;FlyStudioKey飞屋软件技术，孕育创新，绽放梦想！
                     我们匠心制作每一款产品，用技术打磨出无限可能。
                     在游戏世界的奇妙领域中，我们用精湛的软件技术为你打造无限畅想的奇迹。
                     一起，飞向未知的高度！</p>' />
     <br />
-    <Body_LIRT message="<br /><h4>Quickly Minecraft Launcher(qmcl)启动器</h4>
+    <Body_LIRT v-if="md.mobile()" message="<br /><h4>Quickly Minecraft Launcher(qmcl)启动器</h4>
+                        <p>新一代跨平台启动器，项目大重构！</p>"
+               img_url="res/icon.png"
+               to_url="/app/qmcl"
+               margin_left="1%"/>
+    <Body_LIRT v-else message="<br /><h4>Quickly Minecraft Launcher(qmcl)启动器</h4>
                         <p>新一代跨平台启动器，项目大重构！</p>"
                img_url="res/icon.png"
                to_url="/app/qmcl"
                margin_left="-12.5%"/>
-    <Footer is_foot_d="is_foot_d" />
+    <Footer v-if="md.mobile()" />
+    <Footer v-else is_foot_d="is_foot_d" />
   </main>
 </template>
