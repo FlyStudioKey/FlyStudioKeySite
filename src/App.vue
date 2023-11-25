9038ebfe5187e2ef1c673fb15613c81942183310
weiwei <weiwei@mail.flystudiokey.cn>
2023-11-25 20:00:36 +0800

    v0.1.0-test.1 fix: 添加统计功能

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
   <div id="app">
     <router-view></router-view>
   </div>
-  <script async src="https://umami.flystudiokey.cn/script.js" data-website-id="9f53df39-d70a-45d7-909d-bc54f1b3c73f"></script>
 </template>
 
 <script>
